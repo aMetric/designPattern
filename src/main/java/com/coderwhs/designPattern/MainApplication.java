14ee7125f71877a109e39ddecdef7160a0f459b9
wuhs <wuhs@si-tech.com.cn>
2024-06-23 10:28:57 +0800

    解决redis报错NOAUTHAuthentication required问题

--- src/main/java/com/coderwhs/designPattern/MainApplication.java
+++ src/main/java/com/coderwhs/designPattern/MainApplication.java
@@ -13,8 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * @author coderwhs
  *
  */
-// todo 如需开启 Redis，须移除 exclude 中的内容
-@SpringBootApplication(exclude = {RedisAutoConfiguration.class})
+@SpringBootApplication()
 @MapperScan("com.coderwhs.designPattern.mapper")
 @EnableScheduling
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
